ae3b6abc197a3fa3fd6a7dcab2f06784de4357f8
Shaikh Rahman <shaikh.s.rahman@gmail.com>
2018-05-03 23:59:19 -0500

    added UI styling

---
+++ javascript/app.js
@@ -0,0 +1,20 @@
+$(document).ready( function () {
+    // var database = firebase.database();
+    // database.ref().on("value", function (snapshot) {
+    //     var data = snapshot.val();
+    //     console.log(snapshot.val());
+    // });
+
+    $("#submitButton").on("click", function () {
+        console.log("clicked");
+        var div = $("<div>");
+        div.addClass("row");
+        var col1 = $("<div class='col-md-3'>");
+        var col2 = $("<div class='col-md-2'>");
+        var col3 = $("<div class='col-md-3'>");
+        var col4 = $("<div class='col-md-2'>");
+        var col5 = $("<div class='col-md-2'>");
+        
+    });
+
+});
